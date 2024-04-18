a2fa2ffb55f75f2796285797a8393b3432ef6f4a
Santiago Lopez <153535090+Snatxi@users.noreply.github.com>
2024-04-18 16:32:10 -0400

    added audio manager

---
+++ Assets/_Project/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip[] randomAudioClips;
+    float minTime;
+    float maxTime;
+    float timeToWait;
+    IEnumerator PlayRandomSound()
+    {
+        audioSource.clip = randomAudioClips[Random.Range(0, randomAudioClips.Length)];
+        audioSource.Play();
+        timeToWait = Random.Range(minTime, maxTime);
+        yield return new WaitForSeconds(timeToWait);
+        StartCoroutine(PlayRandomSound());
+    }
+
+    public void Start()
+    {
+        StartCoroutine(PlayRandomSound());
+    }
+}
